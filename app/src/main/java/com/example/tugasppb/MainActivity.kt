9dfb0e28362e936e3191d9a2edf78a3ec89f082d
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-12-20 18:33:01 +0700

    add

--- app/src/main/java/com/example/tugasppb/MainActivity.kt
+++ app/src/main/java/com/example/tugasppb/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.tugasppb
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttoaja?.setOnClickListener {
-            startActivity(Intent(this,PPID::class.java))
+
+    }
+
+    fun clickMenu(it: View) {
+        when (it.id) {
+            berita.id -> Log.d("test","Berita")
+            ppid.id -> Log.d("test","PPid")
+            else -> Toast.makeText(this,"Menu belum terdaftar !",Toast.LENGTH_SHORT).show()
         }
+
     }
 }
