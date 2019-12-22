558d52ae7145c961fe5eaa91cb0c48f0082891b4
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2019-12-23 00:58:18 +0700

    delete swipe

--- app/src/main/java/com/example/tugasppb/utils/RecycleItemTouchHelper.kt
+++ app/src/main/java/com/example/tugasppb/utils/RecycleItemTouchHelper.kt
@@ -25,10 +25,10 @@ class RecycleItemTouchHelper(
         listener?.onSwiped(viewHolder, direction, viewHolder.adapterPosition)
     }
 
-    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
-        val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
-        getDefaultUIUtil().clearView(foregroundView)
-    }
+//    override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+//        val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
+//        getDefaultUIUtil().clearView(foregroundView)
+//    }
 
     override fun onChildDraw(
         c: Canvas,
@@ -50,38 +50,38 @@ class RecycleItemTouchHelper(
             isCurrentlyActive
         )
     }
-
-    override fun onChildDrawOver(
-        c: Canvas,
-        recyclerView: RecyclerView,
-        viewHolder: RecyclerView.ViewHolder?,
-        dX: Float,
-        dY: Float,
-        actionState: Int,
-        isCurrentlyActive: Boolean
-    ) {
-        val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
-        getDefaultUIUtil().onDrawOver(
-            c,
-            recyclerView,
-            foregroundView,
-            dX,
-            dY,
-            actionState,
-            isCurrentlyActive
-        )
-    }
-
-    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
-        if (viewHolder !== null) {
-            val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
-            getDefaultUIUtil().onSelected(foregroundView)
-        }
-    }
-
-    override fun convertToAbsoluteDirection(flags: Int, layoutDirection: Int): Int {
-        return super.convertToAbsoluteDirection(flags, layoutDirection)
-    }
+//
+//    override fun onChildDrawOver(
+//        c: Canvas,
+//        recyclerView: RecyclerView,
+//        viewHolder: RecyclerView.ViewHolder?,
+//        dX: Float,
+//        dY: Float,
+//        actionState: Int,
+//        isCurrentlyActive: Boolean
+//    ) {
+//        val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
+//        getDefaultUIUtil().onDrawOver(
+//            c,
+//            recyclerView,
+//            foregroundView,
+//            dX,
+//            dY,
+//            actionState,
+//            isCurrentlyActive
+//        )
+//    }
+//
+//    override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+//        if (viewHolder !== null) {
+//            val foregroundView: View = (viewHolder as RVListBerita.MyViewHolder).view.view_foregound
+//            getDefaultUIUtil().onSelected(foregroundView)
+//        }
+//    }
+//
+//    override fun convertToAbsoluteDirection(flags: Int, layoutDirection: Int): Int {
+//        return super.convertToAbsoluteDirection(flags, layoutDirection)
+//    }
 }
 
 interface RecyclerItemHelperTouchHelperListener {
